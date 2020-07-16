c8a294e194ef3624547c31ba4530764e5b70547f
jasonmassimino <jasonmassimino@mail.adelphi.edu>
2020-07-16 00:30:15 +0000

    Sprint 3 Final Update

--- setUp.sh
+++ setUp.sh
@@ -4,58 +4,115 @@ ca_key=/etc/kubernetes/pki/ca.key
 ca_crt=/etc/kubernetes/pki/ca.crt
 ca_key_ENC=$(base64 -w 0 $ca_key)
 ca_crt_ENC=$(base64 -w 0 $ca_crt)
-#echo $ca_key_ENC
-#echo $ca_crt_ENC
 
-bash rmuser.sh
+#Used for Testing
+#bash rmuser.sh
 
 server="https://10.13.169.230:6443"
 
 INPUTCSVFILE=./testUsers.csv ## Maybe update this later to take in user input and have a user give the file location of this file.
+DEPLOYMENT=/home/mposada/src/resource/deployment.yml ##Location of deployment file
 
-
-if [! -f $INPUTCSVFILE ] ;
+if [! -f "$INPUTCSVFILE"];
 then
         echo "$INPUTCSVFILE file not found"
         exit 1
 fi
 
+#Prepares Input
 tmp=$(mktemp)
 chmod 600 $tmp
 grep -Ev '^[#;%]' $INPUTCSVFILE |tr -d ' \t' |grep -v '^$' >> $tmp
 INPUTCSVFILE=$tmp
 sh ./create_csv.sh $INPUTCSVFILE ## This will create the users and the grouping
 
+mkdir tempKube ##Makes directory to hold kube files
+
+#makes list of unique groups
+tmp2=$(mktemp)
+for u in $(cat $INPUTCSVFILE)
+do
+        g="$(echo $u | cut -d "," -f 2)"
+        echo "$g" >> $tmp2
+done
+
+uniqueGroups=$( cat $tmp2 | sort | uniq)
+
+#Processes Groups
+for u in $(echo $uniqueGroups)
+do
+        echo "Creating the namespace for $u"
+        sudo -u mposada kubectl create namespace $u
+
+        echo "Deploying $u"
+        sudo -u mposada kubectl apply -n $u -f $DEPLOYMENT
+
+        roleconfig="apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+    namespace: $u
+    name: $u-role
+rules:
+- apiGroups: ['*']
+  resources: ['*']
+  verbs: ['*']"
+
+        #Write to File
+        echo "Making role-deployment for $u"
+        echo "$roleconfig" > tempKube/role_$u-deployment-manager.yml
+
+        #Apply Role
+        echo "Applying role-deployment for $u"
+        sudo -u mposada kubectl apply -f tempKube/role_$u-deployment-manager.yml
+
+        rolebindingconfig="apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+    name: $u-rolebinding
+    namespace: $u
+roleRef:
+  kind: Role
+  name: $u-role
+  apiGroup: rbac.authorization.k8s.io
+subjects:"
+
+        #Apply Template
+        echo "Applying Template rolebinding for $u"
+        echo "$rolebindingconfig" > tempKube/rolebinding_$u-deployment-manager.yml
+
+done
+
+
 
 for u in $(cat $INPUTCSVFILE)
 do
-    groupName="$(echo $u | cut -d "," -f 2)"
-    usersName="$(echo $u | cut -d "," -f 1)"
-    echo "The username is : $usersName"
+        groupName="$(echo $u | cut -d "," -f 2)"
+        usersName="$(echo $u | cut -d "," -f 1)"
+        echo "The username is : $usersName"
 
-    #kubectl create namespace $groupName
+        #kubectl create namespace $groupName
 
-    echo "Creating a Private Key "
-    #Make user key and csr
-    openssl genrsa -out $usersName.key 2048
-    openssl req -new -key $usersName.key -out $usersName.csr -subj "/CN=$usersName/O=$groupName"
+        echo "Creating a Private Key "
+        #Make user key and csr
+        openssl genrsa -out $usersName.key 2048
+        openssl req -new -key $usersName.key -out $usersName.csr -subj "/CN=$usersName/O=$groupName"
 
-    #make user crt
-    openssl x509 -req -in $usersName.csr -CA $ca_crt -CAkey $ca_key -CAcreateserial -out $usersName.crt -days 500
+        #make user crt
+        openssl x509 -req -in $usersName.csr -CA $ca_crt -CAkey $ca_key -CAcreateserial -out $usersName.crt -days 500
 
 
-    usr_key_ENC=$(base64 -w 0 $usersName.key)
-    usr_crt_ENC=$(base64 -w 0 $usersName.crt)
+        usr_key_ENC=$(base64 -w 0 $usersName.key)
+        usr_crt_ENC=$(base64 -w 0 $usersName.crt)
 
-    echo "We are now creating and moving files."
-    mkdir /home/$usersName/.certs && mv $usersName.csr $usersName.crt $usersName.key /home/$usersName/.certs
+        echo "We are now creating and moving files."
+        mkdir /home/$usersName/.certs && mv $usersName.csr $usersName.crt $usersName.key /home/$usersName/.certs
 
-    echo "Creating the .kube directory"
-    mkdir -v /home/$usersName/.kube
+        echo "Creating the .kube directory"
+        mkdir -v /home/$usersName/.kube
 
-    #Set context
-    sudo -u mposada kubectl config set-context "$groupName-context" --cluster=kubernetes --user=$usersName --namespace=$groupName
-userconfig="apiVersion: v1
+        #Set context
+        sudo -u mposada kubectl config set-context "$groupName-context" --cluster=kubernetes --user=$usersName --namespace=$groupName
+        userconfig="apiVersion: v1
 clusters:
 - cluster:
     certificate-authority-data: $certificate_data
@@ -76,60 +133,36 @@ users:
     client-certificate: /home/$usersName/.certs/$usersName.crt
     client-key: /home/$usersName/.certs/$usersName.key"
 
-#Write to File
-echo "Writing to config"
-echo "$userconfig" >> /home/$usersName/.kube/config
+        #Write to File
+        echo "Writing to config"
+        echo "$userconfig" >> /home/$usersName/.kube/config              
 
-roleconfig="apiVersion: rbac.authorization.k8s.io/v1
-kind: Role
-metadata:
-    namespace: $groupName
-    name: $groupName-role
-rules:
-- apiGroups: ['*']
-  resources: ['*']
-  verbs: ['*']"
-
-#Write to File
-echo "Writing to role-deployment-manager"
-echo "$roleconfig" >> /home/$usersName/.kube/role-deployment-manager.yml
-
-rolebindingconfig="apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
-metadata:
-    name: $groupName-rolebinding
-    namespace: $groupName
-subjects:
-- kind: User
+        #Adds user to binding file
+        userBinding="- kind: User
   name: $usersName
-  apiGroup:
-roleRef:
-  kind: Role
-  name: $groupName-role
-  apiGroup: rbac.authorization.k8s.io"
+  apiGroup:"
 
-#Write to File
-echo "Writing to rolebinding-deployement-manager"
-echo "$rolebindingconfig" >> /home/$usersName/.kube/rolebinding-deployment-manager.yml
+        echo "$userBinding" >> tempKube/rolebinding_$groupName-deployment-manager.yml
 
-echo "Creating the namespace for the group"
-sudo -u mposada kubectl create namespace $groupName
+        #Performs relevant bindings of kube files
+        echo "Manually setting certificates"
+        sudo -u mposada kubectl config set-credentials $usersName --client-certificate=/home/$usersName/.certs/$usersName.crt  --client-key=/home/$usersName/.certs/$usersName.key
 
-echo "Manually setting certificates"
-sudo -u mposada kubectl config set-credentials $usersName --client-certificate=/home/$usersName/.certs/$usersName.crt  --client-key=/home/$usersName/.certs/$usersName.key
-
-
-echo "Create the role deployment"
-sudo -u mposada kubectl create -f /home/$usersName/.kube/role-deployment-manager.yml
-#sudo -u mposada kubectl apply -f /home/$usersName/.kube/role-deployment-manager.yml
-echo "Create the role-binding"
-#sudo -u mposada kubectl create -f /home/$usersName/.kube/rolebinding-deployment-manager.yml
-sudo -u mposada kubectl apply -f /home/$usersName/.kube/rolebinding-deployment-manager.yml
-echo "Giving the user a deployment!"
-sudo -u mposada kubectl apply -n group666 -f /home/mposada/src/resource/deployment.yml
-echo "Giving the user's rights for the new files that were created"
-sudo chown -R $usersName:$groupName /home/$usersName/
+        echo "Giving the user's rights for the new files that were created"
+        sudo chown -R $usersName:$groupName /home/$usersName/
 done
 
-#Exit
+##Pushes Binding for all groups
 
+for u in $(echo $uniqueGroups)
+do
+        echo "Applying rolebinding for $u"
+        sudo -u mposada kubectl apply -f tempKube/rolebinding_$u-deployment-manager.yml
+done
+
+#Cleans the Temporary Files
+
+rm -rf tempKube
+
+#Exit
+                                                                                       
